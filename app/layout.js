c6c898c0bef19be363e72d69a4009419d4166ac3
sour <samuel.lindeyer@gmail.com>
2024-08-14 11:05:35 +1000

    nothing

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const font = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+const font1 = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
   title: "Podomoro timer",
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className = {`${font1.className}`}>{children}</body>
     </html>
   );
 }
